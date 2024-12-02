d7a4c20e8d2f445f0aa35e0b4bdb5b35dff2c6f6
AmeyaAdam <ameyaadamcollege@gmail.com>
2024-12-02 20:57:59 +0530

    Intial commit

---
+++ arrays-and-arraylist/IntroToArray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IntroToArray {
+    public static void main(String[] args) {
+        
+        // Declaring an Array
+        int[] array = new int[4];
+
+        // Taking Input 
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter 4 elements of Array: ");
+        for (int i=0; i<array.length; i++)
+        {
+            array[i] = sc.nextInt();
+        }
+
+        // Printing contents of Array
+        System.out.println("Array elements are: ");
+        int size = array.length;
+        for (int i=0; i<size;i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+
+    }
+}
