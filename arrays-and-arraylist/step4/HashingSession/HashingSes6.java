fa0420752f3385a0fba9e8417bf42dec5b4f9cc1
aacodes <aacodes00@gmail.com>
2024-12-25 22:28:38 +0530

    Hashing Step4 25-12-24

---
+++ arrays-and-arraylist/step4/HashingSession/HashingSes6.java
@@ -0,0 +1,38 @@
+package step4.HashingSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashingSes6 {
+    public static void main(String[] args) {
+        //Find count of number of subarrays with sum ==  k 
+
+        int[] arr = {1,2,3,4,5};
+        int k = 5;
+
+        System.out.println("Num of subarrays with sum " + k + ": " + countSubarraysOptimized(arr, k));
+    }
+
+    public static int countSubarraysOptimized(int[] arr, int k){
+
+        int sum = 0;
+        int count = 0;
+
+        Map<Integer, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0,1);
+
+        for(int num : arr) {
+            sum += num;
+
+            if(prefixSumCount.containsKey(sum - k)){
+                count += prefixSumCount.get(sum - k);
+            }
+
+            prefixSumCount.put(sum, prefixSumCount.getOrDefault(sum, 0) + 1);
+        }
+
+
+        return count;
+    }
+    
+}
