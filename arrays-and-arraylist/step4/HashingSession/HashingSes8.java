01e2c4cc669852e94488efa9977eaa55c9746b69
aacodes <aacodes00@gmail.com>
2024-12-30 20:03:16 +0530

    S4 Hashing 30-12-24

--- arrays-and-arraylist/step4/HashingSession/HashingSes8.java
+++
@@ -1,8 +0,0 @@
-package step4.HashingSession;
-
-public class HashingSes8 {
-    public static void main(String[] args) {
-        //Find count of shortest/largest subarrays with sum k in given array
-    }
-    
-}
---
+++ arrays-and-arraylist/step4/HashingSession/HashingSes8_BF.java
@@ -0,0 +1,69 @@
+package step4.HashingSession;
+
+public class HashingSes8_BF {
+    public static void main(String[] args) {
+        //Find count of shortest/largest subarrays with sum k in given array
+        int[] nums = {1,2,3,4,2,5};
+        int k = 5;
+
+        System.out.println("Shortest subarrays with sum " + k + ": " + countShortestSubarraysWithSumK(nums, k));
+        System.out.println("Largest subarrays with sum " + k + ": " + countLargestSubarraysWithSumK(nums, k));
+    }
+
+    public static int countShortestSubarraysWithSumK(int[] nums, int k){
+        int n = nums.length;
+        int minLength = Integer.MAX_VALUE;
+        int count = 0;
+
+        for(int i = 0; i<n; i++){
+            int sum = 0;
+
+            for(int j=i; j<n; j++){
+                sum += nums[j];
+
+                if(sum == k){
+                    int length = j - i + 1;
+
+                    if(length < minLength){
+                        minLength = length;
+                        count = 1;
+
+                    } else if (length == minLength){
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static int countLargestSubarraysWithSumK(int[] nums, int k){
+        int n = nums.length;
+        int maxLength = -1;
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            int sum = 0;
+
+            for(int j=i; j<n; j++){
+                sum += nums[j];
+
+                if(sum == k){
+                    int length = j-i+1;
+                    
+                    if(length > maxLength){
+                        maxLength = length;
+                        count = 1;
+                    } else if (length == maxLength) {
+                        count++;
+                    }
+                }
+
+            }
+        }
+
+        return count;
+    }
+    
+}
