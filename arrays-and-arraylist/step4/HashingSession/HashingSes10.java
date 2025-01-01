914cdf2389c232e640c79efc4dce8aa1272bba00
aacodes <aacodes00@gmail.com>
2025-01-01 22:34:43 +0530

    S4 Hashing 01-01-2025

---
+++ arrays-and-arraylist/step4/HashingSession/HashingSes10.java
@@ -0,0 +1,8 @@
+package step4.HashingSession;
+
+public class HashingSes10 {
+    public static void main(String[] args) {
+        // Session 10
+    }
+    
+}
--- arrays-and-arraylist/step4/HashingSession/HashingSes9.java
+++ arrays-and-arraylist/step4/HashingSession/HashingSes9.java
@@ -1,6 +1,5 @@
 package step4.HashingSession;
 
-import java.util.HashMap;
 import java.util.*;
 
 public class HashingSes9 {
@@ -11,14 +10,17 @@ public class HashingSes9 {
 
         Map<Character, Integer> count = new HashMap<>();
 
+        // Count the frequency of characters in string s
         for(char x : s.toCharArray()) {
             count.put(x, count.getOrDefault(x, 0) + 1);
         }
 
+        // Decrement the frequency of characters in string t
         for(char x : t.toCharArray()) {
             count.put(x, count.getOrDefault(x, 0) - 1);
         }
 
+        // Check if any character has non-zero frequency
         for(int val : count.values()){
             if(val != 0){
                 return false;
